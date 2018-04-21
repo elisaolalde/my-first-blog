739eb111190b90a8b70c633a28b33385cda2ee01
elisaolalde <elisa.olalder@gmail.com>
2018-04-21 14:15:39 +0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,18 @@
+print('Hello, Django girls!')
+def hi(name):
+    if name == "Elisa":
+        print("Hey Elisa")
+    elif name == "Fernando":
+        print("Hey Nando!")
+    else:
+        print("Hi stalker!")
+hi("Fernando")
+def hi(name):
+    print("Hi " + name + "!")
+
+girls = ["Rachel", "Monica", "Phoebe", "Eisa"]
+for name in girls:
+    hi(name)
+    print("Next girl")
+for i in range(1,6):
+    print(i)
